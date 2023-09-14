cb7f4da9255edf22576c045d174548bdfde1316f
julia <julie.tataurova@yandex.ru>
2023-09-14 16:04:41 +0300

    quick sort works correctly

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -1,123 +1,77 @@
-#include "quick_sort.h"
+#include <stdio.h>
 #include <assert.h>
 
-size_t partition(int *data, size_t data_size)
+void swap(int *data, size_t index1, size_t index2)
+{
+    int tmp = *(data + index2);
+    *(data + index2) = *(data + index1);
+    *(data + index1) = tmp;
+}
+
+size_t partition(int *data, size_t data_size) // sorts numbers relative to the middle
 {
     assert(data != NULL);
 
     size_t right = data_size - 1;
     size_t left = 0;
     size_t mid = data_size/2;
-    int is_sorted = 0;
 
-    // printf("mid = %zu\n", mid);
-
-    while(left <= right && is_sorted < 2)
+    while(1)
     {
         right = data_size - 1;
         left = 0;
         mid = data_size/2;
 
-        while((*(data + right) >= *(data + mid)) && right > mid)
+        while((*(data + right) >= *(data + mid)) && right > mid) //getting closer to the middle from the right
         {
             right--;
         }
 
-        while((*(data + left) <= *(data + mid)) && left < mid)
+        while((*(data + left) <= *(data + mid)) && left < mid) // getting closer to the middle from the left
         {
             left++;
         }
 
-        // printf("left = %zu  right = %zu\n", left, right);
-
-        if((left < right))
+        if(left == mid && right == mid) // checks if an arrya is already sorted
         {
-            int tmp = *(data + right);
-            *(data + right) = *(data + left);
-            *(data + left) = tmp;
+            break;
+        }
 
-            (right > mid) ? (right--) : 1;
+        swap(data, left, right);
 
-            (left < mid) ? (left++) : 1;
+        if(right > mid) right--;
+        if(left < mid) left++;
     
-        }
-
-        if(left == right)
-        {
-            is_sorted++;
-        }
-
-        // for(size_t i = 0; i < data_size; i++)
-        // {
-        //     printf("%d ", data[i]);
-        // }
-        // printf("\ntttttttttttt mid = %zu ===============\n", right);
     }
 
     return mid;
 }
 
-void simpleSort(int *data)
+void simpleSort(int *data) // compare two remaining elements of the array
 {
     assert(data != NULL);
 
     if(*(data) > *(data + 1))
     {
-        int tmp = *data;
-        *(data) = *(data + 1);
-        *(data + 1) = tmp;
+        swap(data, 1, 0);
     }
 }
 
 int Sort(int *data, size_t data_size)
 {
     assert(data != NULL);
-    
+
     size_t mid = partition(data, data_size);
 
-    if(mid > 1)
+    if(data_size > 5)
     {
-        Sort(data, data_size - mid);
-        // puts("here");
-        Sort(data + mid + 1, mid - 1);
+        Sort(data, mid);
+        Sort(data + mid + 1, mid - (1 - (data_size % 2))); 
     }
 
-    // printf("%d %d\n", data[0], data[1]);
     simpleSort(data);
+    simpleSort(data + (data_size%2) + 2);
 
     return 0;
 }
 
-// int main()
-// {
-//     // int data[] = {8, 3, 2, 6, 3, 7, 4, 5};
-//     // int data[] = {8, 3, 9, 10, 3, 7, 4, 5};
-//     // int data[] = {1, 1, 1, 1, 1, 1, 1, 1};
-//     // int data[] = {1, 2, 1, 2, 1, 2, 1, 2};
-//     // int data[] = {1, 1, 1, 1, 2, 2, 2, 2};
-//     int data[] = {8, 3, 2, 6, 3, 7, 4};
-//     size_t data_size = sizeof(data)/sizeof(int);
-
-//     for(size_t i = 0; i < data_size; i++)
-//     {
-//         printf("%d ", data[i]);
-//     }
-//     printf("\n-------------------------------------\n");
-
-//     // size_t mid = partition(data, data_size);
-//     // for(size_t i = 0; i < data_size; i++)
-//     // {
-//     //     printf("%d ", data[i]);
-//     // }
-//     // printf("\n------------\n");
-//     // printf("mid = %zu\n", mid);
-
-//     Sort(data, data_size);
-//     for(size_t i = 0; i < data_size; i++)
-//     {
-//         printf("%d ", data[i]);
-//     }
-//     printf("\n");
-    
-//     return 0;
-// }
