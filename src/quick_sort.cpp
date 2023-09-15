656f6cf4cfb22da3ce1fa2f280ff882424d605af
julia <julie.tataurova@yandex.ru>
2023-09-15 04:10:03 +0300

    quick sort

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -8,19 +8,26 @@ void swap(int *data, size_t index1, size_t index2)
     *(data + index1) = tmp;
 }
 
-size_t partition(int *data, size_t data_size) // sorts numbers relative to the middle
+void simpleSort(int *data) // compare two remaining elements of the array
 {
     assert(data != NULL);
 
-    size_t right = data_size - 1;
+    if(*(data) > *(data + 1))
+    {
+        swap(data, 1, 0);
+    }
+}
+
+size_t partition(int *data, size_t data_size)
+{
+    assert(data != NULL);
+
     size_t left = 0;
+    size_t right = data_size - 1;
     size_t mid = data_size / 2;
 
     while(1)
     {
-        right = data_size - 1;
-        left = 0;
-        mid = data_size / 2;
 
         while((*(data + right) >= *(data + mid)) && right > mid) //getting closer to the middle from the right
         {
@@ -32,47 +39,55 @@ size_t partition(int *data, size_t data_size) // sorts numbers relative to the m
             left++;
         }
 
-        if(left == mid && right == mid) // checks if an arrya is already sorted
+        if(left == right)
         {
-            break;
+            break; 
         }
-
+        
         swap(data, left, right);
 
-        if(right > mid) right--;
-        if(left < mid) left++;
-    
+        if(right == mid)
+        {
+            mid = left;
+        }
+        else if(left == mid)
+        {
+            left = right;
+            mid = left;
+        }
+
     }
 
     return mid;
 }
 
-void simpleSort(int *data) // compare two remaining elements of the array
-{
-    assert(data != NULL);
-
-    if(*(data) > *(data + 1))
-    {
-        swap(data, 1, 0);
-    }
-}
 
 int Sort(int *data, size_t data_size)
 {
-    assert(data != NULL);
-
-    const size_t el_limit = 5;
     size_t mid = partition(data, data_size);
 
-    if(data_size > el_limit)
+    if(data_size > 2)
     {
-        Sort(data, mid);
-        Sort(data + mid + 1, mid - (1 - (data_size % 2))); 
+        if(mid == 0)
+        {
+            Sort(data + 1, data_size - 1);
+        }
+        else if(mid == data_size - 1)
+        {
+            Sort(data, data_size - 1);
+        }
+        else
+        {
+            Sort(data, mid);
+            Sort(data + mid + 1, data_size - mid - 1);
+        }      
     }
 
-    simpleSort(data);
-    simpleSort(data + (data_size % 2) + 2);
-
+    if(data_size == 2)
+    {
+        simpleSort(data);
+    }
+    
     return 0;
 }
 
