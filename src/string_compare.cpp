8de886ce84d48a5606f83242cd4c7937d550307d
julia <julie.tataurova@yandex.ru>
2023-09-18 01:56:49 +0300

    i instead of i1 and i2 in compare f

--- src/string_compare.cpp
+++ src/string_compare.cpp
@@ -22,30 +22,30 @@ int string_compare_right_left(const void *line1, const void *line2)
     assert(line1 != NULL);
     assert(line2 != NULL);
 
-    const char *line11 = (const char *)(*((char **)line1));
-    const char *line22 = (const char *)(*((char **)line2));
+    const char *line11 = *((const char **)line1);
+    const char *line22 = *((const char **)line2);
 
     size_t len1 = strlen(line11) - 1;
     size_t len2 = strlen(line22) - 1;
-    size_t i1 = 0;
-    size_t i2 = 0;
+    size_t i = 0;
 
-    while((i1 <= len1) && (i2 <= len2))
+    const char *l1 = line11 + len1 - i;
+    const char *l2 = line22 + len2 - i;
+
+    while((i <= len1) && (i <= len2))
     {
-        const char *l1 = line11 + len1 - i1;
-        const char *l2 = line22 + len2 - i2;
 
         if((*l1 != *l2) && isalpha(*l1) && isalpha(*l2))
         {
-            return *(const unsigned char *)(line11 + len1 - i1) - *(const unsigned char *)(line22 + len2 - i2);
+            return *l1 - *l2;
         }
 
-        if(isalpha(*l1) && !isalpha(*l2)) i2++;
-        else if(!isalpha(*l1) && isalpha(*l2)) i1++;
+        if(!isalpha(*l1)) *l1--;
+        else if(!isalpha(*l2)) *l2--;
         else
         {
-            i1++;
-            i2++;
+            *l1--;
+            *l2--;
         }
     }
 
@@ -67,25 +67,25 @@ int string_compare_right_left_qs(const char *line11, const char *line22)
 
     size_t len1 = strlen(line11) - 1;
     size_t len2 = strlen(line22) - 1;
-    size_t i1 = 0;
-    size_t i2 = 0;
+    size_t i = 0;
 
-    while((i1 <= len1) && (i2 <= len2))
+    const char *l1 = line11 + len1 - i;
+    const char *l2 = line22 + len2 - i;
+
+    while((i <= len1) && (i <= len2))
     {
-        const char *l1 = line11 + len1 - i1;
-        const char *l2 = line22 + len2 - i2;
 
         if((*l1 != *l2) && isalpha(*l1) && isalpha(*l2))
         {
-            return *(const unsigned char *)(line11 + len1 - i1) - *(const unsigned char *)(line22 + len2 - i2);
+            return *l1 - *l2;
         }
 
-        if(isalpha(*l1) && !isalpha(*l2)) i2++;
-        else if(!isalpha(*l1) && isalpha(*l2)) i1++;
+        if(!isalpha(*l1)) *l1--;
+        else if(!isalpha(*l2)) *l2--;
         else
         {
-            i1++;
-            i2++;
+            *l1--;
+            *l2--;
         }
     }
 
